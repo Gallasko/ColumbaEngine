0b15780cb70b46534b1228ef0e80a9618d28dc98
Gallasko <gallasko@gmail.com>
2025-09-18 11:49:24 +0200

    Connected the lexer to the compiler's input handling.

--- exemples/PgCompiler/compiler.h
+++ exemples/PgCompiler/compiler.h
@@ -1,12 +1,45 @@
 #pragma once
 
-#include "Interpreter/token.h"
+#include "Interpreter/lexer.h"
+
+#include <iostream>
+#include <iomanip>
 
 namespace pg
 {
-
     struct Compiler
     {
+        void compile(const std::string& source)
+        {
+            Lexer lexer;
 
+            lexer.readFromText(source);
+
+            auto tokens = lexer.getTokens();
+
+            int line = -1;
+
+            while (not tokens.empty())
+            {
+                Token token = tokens.front();
+                tokens.pop();
+
+                if (static_cast<int>(token.line) != line)
+                {
+                    std::cout << std::setw(4) << token.line << " ";
+                    line = token.line;
+                }
+                else
+                {
+                    std::cout << "   | ";
+                }
+
+                std::cout << std::setw(4) << token.column << " ";
+                std::cout << token.text << std::endl;
+
+                if (token.type == TokenType::ENDOFFILE)
+                    break;
+            }
+        }
     };
 }
--- src/Engine/Interpreter/token.h
+++ src/Engine/Interpreter/token.h
@@ -39,7 +39,7 @@ namespace pg
         END    = ';',
         EOL    = '\n',
 
-        //Two characters Operators
+        // Two characters Operators
 
         PLUSEQUAL  = 270,
         MINUSEQUAL = 271,
@@ -95,17 +95,18 @@ namespace pg
 
     struct Token
     {
-        Token(const TokenType& type, const std::string& text, unsigned line, unsigned column) : type(type), text(text), line(line), column(column) { }
-        Token(const Token& other) : type(other.type), text(other.text), line(other.line), column(other.column) { }
+        Token(const TokenType& type, const std::string& text, unsigned line, unsigned column, unsigned length = 0) : type(type), text(text), line(line), column(column), length(length) { }
+        Token(const Token& other) : type(other.type), text(other.text), line(other.line), column(other.column), length(other.length) { }
 
-        Token() : Token(TokenType::INVALID, "", 0, 0) { }
+        Token() : Token(TokenType::INVALID, "", 0, 0, 0) { }
 
-        void operator=(const Token& other) { type = other.type; text = other.text; line = other.line; column = other.column; }
+        void operator=(const Token& other) { type = other.type; text = other.text; line = other.line; column = other.column; length = other.length; }
 
         TokenType type;
         std::string text;
         unsigned line;
         unsigned column;
+        unsigned length = 0;
     };
 
 }
