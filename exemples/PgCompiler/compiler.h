4492b8fe061ef052666bb785bd55136ff17b6f9a
Gallasko <gallasko@gmail.com>
2025-09-18 16:24:08 +0200

    First working parser

--- exemples/PgCompiler/compiler.h
+++ exemples/PgCompiler/compiler.h
@@ -2,16 +2,24 @@
 
 #include "Interpreter/token.h"
 
+#include "chunk.h"
+
+#include "parser.h"
+
 #include <iostream>
 #include <iomanip>
 #include <queue>
 
+#define DEBUG_PRINT_CODE
+
 namespace pg
 {
-    struct Chunk;
-
     struct Compiler
     {
         bool compile(std::queue<Token> tokens, Chunk& chunk);
+
+        void printTokens(std::queue<Token> tokens);
+
+        Parser parser;
     };
 }
