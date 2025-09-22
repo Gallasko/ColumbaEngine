8edd71c428c462a051b887726101f05cce898092
Gallasko <gallasko@gmail.com>
2025-09-22 16:33:27 +0200

    Added get global variables

--- exemples/PgCompiler/compiler_debug.cpp
+++ exemples/PgCompiler/compiler_debug.cpp
@@ -115,6 +115,9 @@ namespace pg
             case OpCode::OP_Define_Global:
                 return simpleInstruction("OP_Define_Global", offset);
 
+            case OpCode::OP_Get_Global:
+                return simpleInstruction("OP_Get_Global", offset);
+
             default:
                 std::cout << "Unknown opcode " << static_cast<uint8_t>(instruction) << std::endl;
                 return offset + 1;
