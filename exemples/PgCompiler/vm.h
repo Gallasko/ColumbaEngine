dad8423efd2acd223c5d9f7049087bfc89ab7d85
Gallasko <gallasko@gmail.com>
2025-09-25 01:43:57 +0200

    Profile the optimization pass

--- exemples/PgCompiler/vm.h
+++ exemples/PgCompiler/vm.h
@@ -16,7 +16,7 @@
 // Todo add this as a flag in when compiling in debug
 // #define DEBUG_TRACE_EXECUTION
 
-// #define DEBUG_PROFILE_COMPILE
+#define DEBUG_PROFILE_COMPILE
 
 #define EMIT_RUNTIME_ERROR(msg) do {runtimeError((Strfy() << msg).getData()); return InterpretResult::RUNTIME_ERROR;} while(0);
 
