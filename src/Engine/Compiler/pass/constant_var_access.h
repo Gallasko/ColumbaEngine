9081383ab33bf6f1d5797ef005e778dcda0348f9
Gallasko <gallasko@gmail.com>
2025-12-10 14:59:44 +0100

    Added Get and Set Constant Globals

--- src/Engine/Compiler/pass/constant_var_access.h
+++ src/Engine/Compiler/pass/constant_var_access.h
@@ -40,6 +40,38 @@ namespace pg
                 return newBytecode;
             });
 
+            pattern = {
+                PatternElement::match(OpCode::OP_Constant, true),
+                PatternElement::match(OpCode::OP_Constant, true),
+                PatternElement::match(OpCode::OP_Set_Global),
+            };
+
+            rewriter->addAdvancedRule(pattern, [](const std::vector<CapturedInstruction>& captured) -> std::vector<uint8_t> {
+                // Create new bytecode sequence for optimized addition
+                std::vector<uint8_t> newBytecode;
+                newBytecode.push_back(static_cast<uint8_t>(OpCode::OP_Set_Constant_Global));
+
+                newBytecode.push_back(captured[0].operands[0]);
+                newBytecode.push_back(captured[1].operands[0]);
+
+                return newBytecode;
+            });
+
+            pattern = {
+                PatternElement::match(OpCode::OP_Constant, true),
+                PatternElement::match(OpCode::OP_Get_Global),
+            };
+
+            rewriter->addAdvancedRule(pattern, [](const std::vector<CapturedInstruction>& captured) -> std::vector<uint8_t> {
+                // Create new bytecode sequence for optimized addition
+                std::vector<uint8_t> newBytecode;
+                newBytecode.push_back(static_cast<uint8_t>(OpCode::OP_Get_Constant_Global));
+
+                newBytecode.push_back(captured[0].operands[0]);
+
+                return newBytecode;
+            });
+
             return rewriter->rewrite(chunk);
         }
 
