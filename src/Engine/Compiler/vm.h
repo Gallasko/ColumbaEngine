53029c53415996bc4e59b5209f58956aa7a816ca
Gallasko <gallasko@gmail.com>
2026-02-03 18:35:07 +0100

    Generalized the fix for __get and __set metamethods Split huge vm.cpp into smaller source files

--- src/Engine/Compiler/vm.h
+++ src/Engine/Compiler/vm.h
@@ -119,6 +119,89 @@ namespace pg
         }
     };
 
+    // Forward declarations for operation handlers
+    void op_return(VM* vm);
+    void op_constant(VM* vm);
+    void op_long_constant(VM* vm);
+    void op_add(VM* vm);
+    void op_subtract(VM* vm);
+    void op_multiply(VM* vm);
+    void op_divide(VM* vm);
+    void op_modulo(VM* vm);
+    void op_negate(VM* vm);
+    void op_equal(VM* vm);
+    void op_not_equal(VM* vm);
+    void op_greater(VM* vm);
+    void op_greater_equal(VM* vm);
+    void op_less(VM* vm);
+    void op_less_equal(VM* vm);
+    void op_true(VM* vm);
+    void op_false(VM* vm);
+    void op_not(VM* vm);
+    void op_and(VM* vm);
+    void op_or(VM* vm);
+    void op_pop(VM* vm);
+    void op_get_local(VM* vm);
+    void op_set_local(VM* vm);
+    void op_get_global(VM* vm);
+    void op_define_global(VM* vm);
+    void op_set_global(VM* vm);
+    void op_jump(VM* vm);
+    void op_jump_if_false(VM* vm);
+    void op_loop(VM* vm);
+    void op_long_jump(VM* vm);
+    void op_long_jump_if_false(VM* vm);
+    void op_long_loop(VM* vm);
+    void op_call(VM* vm);
+    void op_invoke(VM* vm);
+    void op_closure(VM* vm);
+    void op_get_upvalue(VM* vm);
+    void op_set_upvalue(VM* vm);
+    void op_close_upvalue(VM* vm);
+    void op_debug_print(VM* vm);
+    void op_post_incr_global(VM* vm);
+    void op_incr_global(VM* vm);
+    void op_post_decr_global(VM* vm);
+    void op_decr_global(VM* vm);
+    void op_post_incr_local(VM* vm);
+    void op_incr_local(VM* vm);
+    void op_post_decr_local(VM* vm);
+    void op_decr_local(VM* vm);
+    void op_class(VM* vm);
+    void op_get_property(VM* vm);
+    void op_set_property(VM* vm);
+    void op_method(VM* vm);
+    void op_short_int(VM* vm);
+
+    void op_pop_n(VM* vm);
+
+    void op_define_constant_global(VM* vm);
+    void op_get_constant_global(VM* vm);
+    void op_set_constant_global(VM* vm);
+
+    void op_add_ll(VM* vm);
+    void op_subtract_ll(VM* vm);
+
+    void op_subtract_lc(VM* vm);
+    void op_subtract_cl(VM* vm);
+
+    // Table operations
+    void op_build_vector(VM* vm);
+    void op_build_table(VM* vm);
+    void op_get_index(VM* vm);
+    void op_set_index(VM* vm);
+
+    // Iterator operations
+    void op_get_iterator(VM* vm);
+    void op_iterator_next(VM* vm);
+    void op_table_size(VM* vm);
+    void op_table_at(VM* vm);
+
+    void op_define_global_non_popping(VM *vm);
+
+    // Module operations
+    void op_import(VM* vm);
+
     struct VM
     {
         VM();
