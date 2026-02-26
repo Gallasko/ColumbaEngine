b8379d84485b9ca9c2d35395df178159eec8ebd1
Gallasko <gallasko@gmail.com>
2026-02-26 17:51:41 +0100

    Added roundedrect.pgcomp All simple2dshapes are now generic render systems

--- src/Engine/2D/simple2dobject.cpp
+++ src/Engine/2D/simple2dobject.cpp
@@ -17,7 +17,7 @@ namespace pg
         static constexpr char const * DOM = "Shape 2D";
     }
 
-    void Simple2DObjectSystem::init()
+    void Simple2DObjectSystem::setup()
     {
         LOG_THIS_MEMBER(DOM);
 
@@ -33,75 +33,9 @@ namespace pg
         simpleShapeMaterial.setSimpleMesh({3, 2, 1, 4});
 
         materialId = masterRenderer->registerMaterial(simpleShapeMaterial);
-
-        auto group = registerGroup<PositionComponent, Simple2DObject>();
-
-        group->addOnGroup([this](EntityRef entity) {
-            LOG_MILE("Simple 2D Object System", "Add entity " << entity->id << " to ui - 2d shape group !");
-
-            auto ui = entity->get<PositionComponent>();
-            auto obj = entity->get<Simple2DObject>();
-
-            entityRenderCalls[entity->id] = createRenderCall(ui, obj);
-            entitiesInRenderGroup.push_back(entity->id);
-            std::sort(entitiesInRenderGroup.begin(), entitiesInRenderGroup.end());
-
-            changed = true;
-        });
-
-        group->removeOfGroup([this](EntitySystem* /*ecsRef*/, _unique_id id) {
-            LOG_MILE("Simple 2D Object System", "Remove entity " << id << " of ui - 2d shape group !");
-
-            entityRenderCalls.erase(id);
-            entitiesInRenderGroup.erase(std::remove(entitiesInRenderGroup.begin(), entitiesInRenderGroup.end(), id), entitiesInRenderGroup.end());
-
-            changed = true;
-        });
     }
 
-    void Simple2DObjectSystem::execute()
-    {
-        if (not changed)
-            return;
-
-        std::vector<_unique_id> updateQueue;
-        std::vector<_unique_id> temp;
-
-        temp.assign(shapeUpdateSet.begin(), shapeUpdateSet.end());
-        std::sort(temp.begin(), temp.end());
-
-        std::set_intersection(entitiesInRenderGroup.begin(), entitiesInRenderGroup.end(),
-                              temp.begin(), temp.end(),
-                              std::back_inserter(updateQueue));
-
-        shapeUpdateSet.clear();
-
-        for (const auto& entityId : updateQueue)
-        {
-            auto entity = ecsRef->getEntity(entityId);
-
-            if (not entity)
-                continue;
-
-            auto ui = entity->get<PositionComponent>();
-            auto obj = entity->get<Simple2DObject>();
-
-            entityRenderCalls[entityId] = createRenderCall(ui, obj);
-        }
-
-        renderCallList.clear();
-
-        renderCallList.reserve(entityRenderCalls.size());
-
-        for (const auto& [id, renderCall] : entityRenderCalls)
-        {
-            renderCallList.push_back(renderCall);
-        }
-
-        finishChanges();
-    }
-
-    RenderCall Simple2DObjectSystem::createRenderCall(CompRef<PositionComponent> ui, CompRef<Simple2DObject> obj)
+    RenderCall Simple2DObjectSystem::createRenderCall(CompRef<Simple2DObject> obj, CompRef<PositionComponent> ui)
     {
         LOG_THIS_MEMBER(DOM);
 
@@ -140,70 +74,11 @@ namespace pg
         return call;
     }
 
-    void Simple2DObjectSystem::onEvent(const PositionComponentChangedEvent& event)
-    {
-        LOG_THIS_MEMBER(DOM);
-
-        onEventUpdate(event.id);
-    }
-
-    void Simple2DObjectSystem::onEvent(const Simple2DObjectChangedEvent& event)
-    {
-        LOG_THIS_MEMBER(DOM);
-
-        onEventUpdate(event.id);
-    }
-
-    void Simple2DObjectSystem::onEventUpdate(_unique_id entityId)
-    {
-        LOG_THIS_MEMBER(DOM);
-
-        shapeUpdateSet.insert(entityId);
-
-        changed = true;
-    }
-
-    // ---------------------------------------------------------------------------
-    // RoundedRect2DObject serialize / deserialize
-    // ---------------------------------------------------------------------------
-
-    template <>
-    void serialize(Archive& archive, const RoundedRect2DObject& value)
-    {
-        LOG_THIS(DOM);
-
-        archive.startSerialization(RoundedRect2DObject::getType());
-
-        serialize(archive, "cornerRadius", value.cornerRadius);
-        serialize(archive, "colors", value.colors);
-
-        archive.endSerialization();
-    }
-
-    template <>
-    RoundedRect2DObject deserialize(const UnserializedObject& serializedString)
-    {
-        LOG_THIS(DOM);
-
-        if (serializedString.isNull())
-        {
-            LOG_ERROR(DOM, "Element is null");
-            return RoundedRect2DObject{};
-        }
-
-        LOG_INFO(DOM, "Deserializing a RoundedRect2DObject");
-
-        auto cornerRadius = deserialize<float>(serializedString["cornerRadius"]);
-        auto colors = deserialize<constant::Vector4D>(serializedString["colors"]);
-
-        return RoundedRect2DObject{cornerRadius, colors};
-    }
-
     // ---------------------------------------------------------------------------
     // RoundedRect2DObjectSystem
     // ---------------------------------------------------------------------------
 
-    void RoundedRect2DObjectSystem::init()
+    void RoundedRect2DObjectSystem::setup()
     {
         LOG_THIS_MEMBER(DOM);
 
@@ -220,70 +95,9 @@ namespace pg
         mat.setSimpleMesh({3, 2, 1, 4, 1});
 
         materialId = masterRenderer->registerMaterial(mat);
-
-        auto group = registerGroup<PositionComponent, RoundedRect2DObject>();
-
-        group->addOnGroup([this](EntityRef entity) {
-            LOG_MILE("Rounded Rect 2D System", "Add entity " << entity->id << " to rounded rect group!");
-            updateQueue.push(entity->id);
-            changed = true;
-        });
-
-        group->removeOfGroup([this](EntitySystem* ecsRef, _unique_id id) {
-            LOG_MILE("Rounded Rect 2D System", "Remove entity " << id << " from rounded rect group!");
-            auto entity = ecsRef->getEntity(id);
-            ecsRef->detach<RoundedRect2DRenderCall>(entity);
-            changed = true;
-        });
     }
 
-    void RoundedRect2DObjectSystem::execute()
-    {
-        if (not changed)
-            return;
-
-        while (not updateQueue.empty())
-        {
-            auto entityId = updateQueue.front();
-
-            auto entity = ecsRef->getEntity(entityId);
-
-            if (not entity)
-            {
-                updateQueue.pop();
-                continue;
-            }
-
-            auto ui  = entity->get<PositionComponent>();
-            auto obj = entity->get<RoundedRect2DObject>();
-
-            if (entity->has<RoundedRect2DRenderCall>())
-            {
-                entity->get<RoundedRect2DRenderCall>()->call = createRenderCall(ui, obj);
-            }
-            else
-            {
-                ecsRef->_attach<RoundedRect2DRenderCall>(entity, createRenderCall(ui, obj));
-            }
-
-            updateQueue.pop();
-        }
-
-        renderCallList.clear();
-
-        const auto& renderCallView = view<RoundedRect2DRenderCall>();
-
-        renderCallList.reserve(renderCallView.nbComponents());
-
-        for (const auto& renderCall : renderCallView)
-        {
-            renderCallList.push_back(renderCall->call);
-        }
-
-        finishChanges();
-    }
-
-    RenderCall RoundedRect2DObjectSystem::createRenderCall(CompRef<PositionComponent> ui, CompRef<RoundedRect2DObject> obj)
+    RenderCall RoundedRect2DObjectSystem::createRenderCall(CompRef<RoundedRect2DObject> obj, CompRef<PositionComponent> ui)
     {
         LOG_THIS_MEMBER(DOM);
 
@@ -315,19 +129,5 @@ namespace pg
         call.data[10] = obj->cornerRadius;
 
         return call;
-    }
-
-    void RoundedRect2DObjectSystem::onEvent(const EntityChangedEvent& event)
-    {
-        LOG_THIS_MEMBER(DOM);
-
-        auto entity = ecsRef->getEntity(event.id);
-
-        if (not entity or not entity->has<RoundedRect2DObject>())
-            return;
-
-        updateQueue.push(event.id);
-
-        changed = true;
     }
 }
--- src/Engine/2D/simple2dobject.h
+++ src/Engine/2D/simple2dobject.h
@@ -4,40 +4,29 @@
 
 #include "position.h"
 
-#include "Renderer/renderer.h"
+#include "Renderer/genericrendersys.h"
 
 #include "pgconstant.h"
 
 #include "Components/Simple2DObject.generated.h"
-
-#include <unordered_set>
+#include "Components/RoundedRect2DObject.generated.h"
 
 // Todo this system can be faulty during scene changing !
 
 namespace pg
 {
-    struct Simple2DObjectSystem : public AbstractRenderer, System<Own<Simple2DObject>, Listener<PositionComponentChangedEvent>, Listener<Simple2DObjectChangedEvent>, InitSys>
+    struct Simple2DObjectSystem : public GenericRenderSystem<Simple2DObject, Simple2DObjectChangedEvent, PositionComponent, PositionComponentChangedEvent>
     {
-        Simple2DObjectSystem(MasterRenderer* masterRenderer) : AbstractRenderer(masterRenderer, RenderStage::Render) { }
+        Simple2DObjectSystem(MasterRenderer* masterRenderer) : GenericRenderSystem(masterRenderer) { }
         virtual ~Simple2DObjectSystem() { }
 
         virtual std::string getSystemName() const override { return "Shape 2D System"; }
 
-        virtual void init() override;
+        virtual void setup() override;
 
-        virtual void execute() override;
-
-        RenderCall createRenderCall(CompRef<PositionComponent> ui, CompRef<Simple2DObject> obj);
-
-        virtual void onEvent(const PositionComponentChangedEvent& event) override;
-        virtual void onEvent(const Simple2DObjectChangedEvent& event) override;
-        void onEventUpdate(_unique_id entityId);
+        virtual RenderCall createRenderCall(CompRef<Simple2DObject> obj, CompRef<PositionComponent> ui) override;
 
         uint64_t materialId = 0;
-
-        std::unordered_set<_unique_id> shapeUpdateSet;
-        std::unordered_map<_unique_id, RenderCall> entityRenderCalls;
-        std::vector<_unique_id> entitiesInRenderGroup;
     };
 
     template <typename Type>
@@ -80,81 +69,18 @@ namespace pg
     // Rounded rectangle
     // ---------------------------------------------------------------------------
 
-    struct RoundedRect2DObject : public Ctor
+    struct RoundedRect2DObjectSystem : public GenericRenderSystem<RoundedRect2DObject, RoundedRect2DObjectChangedEvent, PositionComponent, PositionComponentChangedEvent>
     {
-        RoundedRect2DObject(float cornerRadius = 0.0f) : cornerRadius(cornerRadius) {}
-        RoundedRect2DObject(float cornerRadius, const constant::Vector4D& c) : cornerRadius(cornerRadius), colors(c) {}
-
-        RoundedRect2DObject(const RoundedRect2DObject& rhs)
-            : cornerRadius(rhs.cornerRadius), colors(rhs.colors), viewport(rhs.viewport), id(rhs.id), ecsRef(rhs.ecsRef) {}
-        virtual ~RoundedRect2DObject() {}
-
-        inline static std::string getType() { return "RoundedRect2DObject"; }
-
-        virtual void onCreation(EntityRef entity) { id = entity.id; ecsRef = entity.ecsRef; }
-
-        void setColors(const constant::Vector4D& c)
-        {
-            colors = c;
-            if (ecsRef) ecsRef->sendEvent(EntityChangedEvent{id});
-        }
-
-        void setOpacity(float alpha)
-        {
-            colors.w = alpha;
-            if (ecsRef) ecsRef->sendEvent(EntityChangedEvent{id});
-        }
-
-        void setCornerRadius(float radius)
-        {
-            cornerRadius = radius;
-            if (ecsRef) ecsRef->sendEvent(EntityChangedEvent{id});
-        }
-
-        void setViewport(size_t vp)
-        {
-            if (viewport != vp)
-            {
-                viewport = vp;
-                if (ecsRef) ecsRef->sendEvent(EntityChangedEvent{id});
-            }
-        }
-
-        float cornerRadius = 0.0f;
-        constant::Vector4D colors {255.0f, 255.0f, 255.0f, 255.0f};
-        size_t viewport = 0;
-        _unique_id id;
-        EntitySystem* ecsRef = nullptr;
-    };
-
-    template <>
-    void serialize(Archive& archive, const RoundedRect2DObject& value);
-
-    template <>
-    RoundedRect2DObject deserialize(const UnserializedObject& serializedString);
-
-    struct RoundedRect2DRenderCall
-    {
-        RoundedRect2DRenderCall(const RenderCall& call) : call(call) {}
-        RenderCall call;
-    };
-
-    struct RoundedRect2DObjectSystem : public AbstractRenderer, System<Own<RoundedRect2DObject>, Own<RoundedRect2DRenderCall>, Listener<EntityChangedEvent>, InitSys>
-    {
-        RoundedRect2DObjectSystem(MasterRenderer* masterRenderer) : AbstractRenderer(masterRenderer, RenderStage::Render) {}
+        RoundedRect2DObjectSystem(MasterRenderer* masterRenderer) : GenericRenderSystem(masterRenderer) {}
         virtual ~RoundedRect2DObjectSystem() {}
 
         virtual std::string getSystemName() const override { return "Rounded Rect 2D System"; }
 
-        virtual void init() override;
-        virtual void execute() override;
+        virtual void setup() override;
 
-        RenderCall createRenderCall(CompRef<PositionComponent> ui, CompRef<RoundedRect2DObject> obj);
-
-        virtual void onEvent(const EntityChangedEvent& event) override;
+        virtual RenderCall createRenderCall(CompRef<RoundedRect2DObject> obj, CompRef<PositionComponent> ui) override;
 
         uint64_t materialId = 0;
-        std::queue<_unique_id> updateQueue;
     };
 
     template <typename Type>
